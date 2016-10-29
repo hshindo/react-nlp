132e0f5628ce81e6881044e52948181cf2cbbb4a
nabeix <nabeix@users.noreply.github.com>
2016-10-30 07:43:12 +0900

    add theme features

--- src/View.jsx
+++ src/View.jsx
@@ -1,18 +1,32 @@
 import React from "react";
 import Line from "./Line";
+import currentTheme, {setTheme} from "./Theme";
 
 class View extends React.Component {
   render() {
-    const { data, linum, colors, types, lineBreak } = this.props;
+    const { data, linum, colors, types, lineBreak, theme } = this.props;
+    if (theme) {
+      setTheme(theme);
+    }
     const lines = [];
     data.forEach((line, i) => {
       let num = linum ? i + 1 : null;
+      let bgColor = null;
+      if (currentTheme.stripe) {
+        bgColor = currentTheme.stripeColor[i % 2];
+      }
       lines.push(
-        <Line key={i} text={line.text} annotations={line.annos} colors={colors} types={types} linum={num} lineBreak={lineBreak == null ? true : false} />
+        <Line key={i} text={line.text} annotations={line.annos} colors={colors} types={types} linum={num} lineBreak={lineBreak == null ? true : false} bgColor={bgColor} />
       );
     });
     return (
-      <div>
+      <div style={{
+        fontSize: currentTheme.fontSize,
+        color: currentTheme.color,
+        borderTop: currentTheme.border,
+        borderLeft: currentTheme.border,
+        borderRight: currentTheme.border
+      }}>
         {lines}
       </div>
     );
